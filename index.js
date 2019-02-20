eea01bde5126e0729419c82a69bdeec6257a899d
Bara Limbong <baralimbong@gmail.com>
2019-02-20 16:15:27 +0700

    add new user

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
 // Connection URL
+const url = process.env.URL
+// Database Name
 const dbName = process.env.DB_NAME
 
-// Database Name
-const dbName =
-  // Use connect method to connect to the server
-  MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err)
-    console.log('Connected successfully to server')
-
-    const db = client.db(dbName)
-
-    client.close()
+// Insert New User
+const createNewUser = function(db) {
+  const newUser = {
+    name: 'john wix',
+    age: '30',
+    email: 'Jwix@gmail.com'
+  }
+  db.collection('users').insert(newUser, (err, result) => {
+    if (err) {
+      console.log({
+        message: 'failed'
+      })
+    } else {
+      console.log({
+        message: 'Created new user',
+        result: result
+      })
+    }
   })
+}
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err)
+  console.log('Connected successfully to server')
+
+  const db = client.db(dbName)
+  createNewUser(db)
+
+  client.close()
+})
